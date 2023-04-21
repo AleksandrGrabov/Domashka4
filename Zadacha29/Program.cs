496836200d3b5a97a273cac277f97891ce7032b6
AleksandrGrabov <grabov-it@yandex.by>
2023-04-21 17:33:44 +0300

    Решили задачу 29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите элементы массива через пробел (максимум 8 элементов):");
+            string input = Console.ReadLine();
+            
+            int[] array = input.Split(' ').Take(8).Select(int.Parse).ToArray();
+
+            Console.WriteLine("Введённый массив:");
+            Console.Write("[");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("]");
